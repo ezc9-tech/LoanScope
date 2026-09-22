f1bf540c475a98f408c52364eac44e4927a344de
Elijah Zane Chapman <ezc9@outlook.com>
2026-09-22 14:44:30 -0500

    This adds the bare functionality to store all of our user's finance stats

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,23 +1,37 @@
-// import { useState } from 'react'
+import { useState, useEffect } from 'react'
 
 function App() {
 
-  // const [principal, setPrincipal] = useState(null)
-  // const [interest, setInterest] = useState(null)
-  // const [payment, setPayment] = useState(null)
+  const [principal, setPrincipal] = useState(null)
+  const [interest, setInterest] = useState(null)
+  const [payment, setPayment] = useState(null)
+
+  useEffect(() => {
+    if (principal && interest && payment) {
+      alert(
+        `Principal: ${principal}, Interest: ${interest}, Payment: ${payment}`,
+      );
+    }
+  }, [principal, interest, payment]);
 
   return (
     <>
       <h1>Loan$cope</h1>
-      <div class="input-container">
+      <div className="input-container">
         <label htmlFor="principal">Principal Amount: $</label>
-        <input type="number" name="principal" id="principal" />
+        <input type="number" name="principal" id="principal" onChange={(event) => {
+          setPrincipal(event.target.value)
+        }}/>
         <label htmlFor="interest">Interest Percentage: </label>
         <span>
-          <input type="number" name="interest" id="interest" /> %
+          <input type="number" name="interest" id="interest" onChange={(event) => {
+          setInterest(event.target.value)
+        }}/> %
         </span>
-        <label htmlFor="payment">Principal Amount: $</label>
-        <input type="number" name="payment" id="payment" />
+        <label htmlFor="payment">Payment Amount: $</label>
+        <input type="number" name="payment" id="payment" onChange={(event) => {
+          setPayment(event.target.value)
+        }}/>
       </div>
     </>
   );
