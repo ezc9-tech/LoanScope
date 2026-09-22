d12a0338d049899823bf7b00f3c786e1252a3ec9
Elijah Zane Chapman <ezc9@outlook.com>
2026-09-22 16:54:53 -0500

    This adds the ability to grab the data we need for the graph on the frontend and adds cors on the backend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,35 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect } from "react";
 
 function App() {
-
-  const [principal, setPrincipal] = useState(null)
-  const [interest, setInterest] = useState(null)
-  const [payment, setPayment] = useState(null)
+  const [principal, setPrincipal] = useState(null);
+  const [interest, setInterest] = useState(null);
+  const [payment, setPayment] = useState(null);
+  const [loanData, setLoanData] = useState(null);
 
   useEffect(() => {
+    const fetchLoanData = async () => {
+      try {
+        const response = await fetch("http://127.0.0.1:8000/api/calculate", {
+          method: "POST",
+          headers: {
+            "Content-type": "application/json",
+          },
+          body: JSON.stringify({
+            principal: parseFloat(principal),
+            interest: parseFloat(interest),
+            payment: parseFloat(payment),
+          }),
+        });
+        const data = await response.json();
+        setLoanData(data);
+        console.log("Calculated Data:", data);
+      } catch (error) {
+        console.error("Error fetching loan data:", error);
+      }
+    };
+
     if (principal && interest && payment) {
-      alert(
-        `Principal: ${principal}, Interest: ${interest}, Payment: ${payment}`,
-      );
+      fetchLoanData();
     }
   }, [principal, interest, payment]);
 
@@ -19,22 +38,16 @@ function App() {
       <h1>Loan$cope</h1>
       <div className="input-container">
         <label htmlFor="principal">Principal Amount: $</label>
-        <input type="number" name="principal" id="principal" onChange={(event) => {
-          setPrincipal(event.target.value)
-        }}/>
+        <input type="number" name="principal" id="principal" value={principal} onChange={(event) => setPrincipal(event.target.value)}/>
         <label htmlFor="interest">Interest Percentage: </label>
         <span>
-          <input type="number" name="interest" id="interest" onChange={(event) => {
-          setInterest(event.target.value)
-        }}/> %
+          <input type="number" name="interest" id="interest" value={interest} onChange={(event) => setInterest(event.target.value)}/>%
         </span>
         <label htmlFor="payment">Payment Amount: $</label>
-        <input type="number" name="payment" id="payment" onChange={(event) => {
-          setPayment(event.target.value)
-        }}/>
+        <input type="number" name="payment" id="payment" value={payment} onChange={(event) => setPayment(event.target.value)}/>
       </div>
     </>
   );
 }
 
-export default App
+export default App;
